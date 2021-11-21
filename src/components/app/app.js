2b094b32b249ab24a8af63129eb3eb55382ce356
Lena <lambitska92@gmail.com>
2021-11-21 18:50:26 +0200

    Lifting State Up

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,9 +12,9 @@ class App extends Component{
         super(props);
         this.state = {
             data: [
-                {name:"John", salary: 800, increase:true, id:1 },
-                {name:"Lena", salary: 1500, increase:false, id:2 },
-                {name:"Kostya", salary: 9888, increase:true, id:3}
+                {name:"John", salary: 800, increase:true, rice:false, id:1 },
+                {name:"Lena", salary: 1500, increase:false, rice:false, id:2 },
+                {name:"Kostya", salary: 9888, increase:true, rice:true, id:3}
             ]
         }
         this.maxId = 4;
@@ -36,6 +36,7 @@ class App extends Component{
         const newItem ={
             name, 
             salary,
+            rice: false,
             increase: false,
             id: this.maxId++
         }
@@ -47,10 +48,36 @@ class App extends Component{
         });
     }
 
+    onToggleProp = (id, prop) =>{
+        // this.setState(({data}) =>{
+            // const index = data.findIndex(elem => elem.id ===id);
+
+            // const old = data[index];
+            // const newItem = {...old, increase: !old.increase};
+            // const newArr = [...data.slice(0, index), newItem, ...data.slice(index +1)];
+            // return{
+            //     data: newArr
+            // }
+        // })
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if(item.id === id){
+                    return {...item, [prop]: !item[prop]}
+                    
+                }
+                return item;
+            })
+        }))
+    }
+
+
     render(){
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter( item => item.increase).length;
         return (
             <div className='app'>
-                <AppInfo/>
+                <AppInfo employees= {employees}
+                increased={increased}/>
     
                 <div className="search-panel">
                 <SearchPanel/>
@@ -59,7 +86,8 @@ class App extends Component{
     
                 <EmployersList 
                 data={this.state.data}
-                onDelete = { this.deleteItem }/>
+                onDelete = { this.deleteItem }
+                onToggleProp = {this.onToggleProp}/>
                 <EmployersAddForm 
                onAdd = {this.addItem}/>
             </div>
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,47 +1,27 @@
-import { Component } from 'react';
+
 import './employer-list-item.css';
 
-class EmployersListItem extends Component {
+const EmployersListItem = (props) => {
    
-    constructor(props){
-        super(props);
-        this.state = {
-            increace: false,
-            like: false
-        }
-    }
-   
-    onIncrease = () =>{
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-    putLike = () =>{
-        this.setState(({like}) =>({
-            like: !like
-        }))
-    }
-
-    render(){
-        const {name, salary, onDelete} = this.props;
-        const {increase, like} = this.state;
+        const {name, salary, onDelete, onToggleProp, increase, rise} = props;
         let classNames = "list-group-item d-flex justify-content-between";
         if(increase){
             classNames += ' increase';
         }
-        if(like){
+        if(rise){
             classNames += ' like';
         }
     
     return (
         <li className={classNames}>
             <span className="list-group-item-label"
-            onClick = {this.putLike}>{name}</span>
+            onClick = {onToggleProp} data-toggle ="rise">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm "
-                    onClick = {this.onIncrease}>
+                    onClick = {onToggleProp}
+                    data-toggle ="increase">
                     <i className="fas fa-cookie"></i>
                 </button>
     
@@ -55,7 +35,7 @@ class EmployersListItem extends Component {
     </li>
     
     );
-    }
+
    
 }
 
